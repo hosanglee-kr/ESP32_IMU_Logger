dea3287a24f84613dc97a5ae75731d95440fe0b4
hosanglee-kr <ddongl@gmail.com>
2026-03-13 20:03:36 +0900

    new file:   src/T002/

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,28 @@
 #include <Arduino.h>
 
-// The build version comes from an environment variable. Use the VERSION
-// define wherever the version is needed.
-#define STRINGIZER(arg) #arg
-#define STR_VALUE(arg)	STRINGIZER(arg)
-#define VERSION			STR_VALUE(BUILD_VERSION)
 
-#define S10
-#ifdef S10
-	#include "S10_streams-i2s-webserver_wav_001.h"
+
+#define T001
+#ifdef T001
+	#include "T002/T002_BMI270_Main_EampAll_005.h"
 #endif
 
 void setup() {
 	// delay(5000);
 
 	Serial.begin(115200);
-	Serial.print("Version: ");
-	Serial.println(VERSION);
+	Serial.print("setup: ");
 
-#ifdef S10
-	S10_setup();
-#endif
+	#ifdef T001
+		B10_init();
+	#endif
 
-	Serial.println("11111");
 }
 
 void loop() {
-#ifdef S10
-	S10_loop();
-#endif
+	#ifdef T001
+		B10_run();
+		delay(5);
+	#endif
+
 }
